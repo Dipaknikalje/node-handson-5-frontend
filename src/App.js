9aea52b3488a5960c107aa8d97ab7e5a3a282807
Dipaknikalje <dipaknikalje80@gmail.com>
2023-07-25 21:17:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+const socket = io("https://chat-backend-tp7d.onrender.com/");
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { message });
+    setMessage("");
+  };
+  console.log(message);
+
+  useEffect(() => {
+    socket.on("chat", (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chat app</h1>
+      {chat.map((payload, index) => {
+        return <p key={index}>{payload.message}</p>;
+      })}
+
+      <form onSubmit={sendChat}>
+        <input
+          type="text"
+          name="chat"
+          placeholder="send text"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 }
